4b0b6c1d5e0e3b0aeb6beb0716da03db2d974c7e
Ryan Hartley <ryan.hartley@gmail.com>
2016-01-25 21:58:03 -0700

    Module 3.3.8

--- image_share.js
+++ image_share.js
@@ -1,6 +1,22 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
+  Session.set("imageLimit", 8);
+
+  lastScrollTop = 0;
+  $(window).scroll(function(event) {
+    // test if we are near the bottom of the window
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        // where are we in the page
+        var scrollTop = $(this).scrollTop();
+        // test if we are going down
+        if (scrollTop > lastScrollTop) {
+            Session.set("imageLimit", Session.get("imageLimit") + 4);
+        }
+        lastScrollTop = scrollTop;
+    }
+  });
+
   Accounts.ui.config({
   	passwordSignupFields: "USERNAME_AND_EMAIL"
   });
@@ -11,7 +27,7 @@ if (Meteor.isClient) {
   		if (userFilter) {
   			filter = {createdBy: userFilter};
   		}
-  		return Images.find(filter, {sort:{createdOn: -1, rating: -1}});
+  		return Images.find(filter, {sort:{createdOn: -1, rating: -1}, limit:Session.get("imageLimit")});
   	},
   	filtering_images: function() {
   		return Session.get("userFilter");
@@ -29,15 +45,17 @@ if (Meteor.isClient) {
   		return Meteor.users.findOne({_id: Session.get("userFilter")}).username || "anon";
   	}
   });
-  Template.body.helpers({username: function() {
-    var user = Meteor.user();
-    if (user) {
-        return user.username;
+  Template.body.helpers({
+    username: function() {
+        var user = Meteor.user();
+        if (user) {
+            return user.username;
+        }
+        else {
+            return "anonymous internet user";
+        }
     }
-    else {
-        return "anonymous internet user";
-    }
-  }});
+  });
 
   Template.images.events({
     'click .js-image': function(event) {
