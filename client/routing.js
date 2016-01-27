2053fe7259704d025a10eddefd04df98db134fbe
Ryan Hartley <ryan.hartley@gmail.com>
2016-01-26 22:33:06 -0700

    Module 3.4.7

--- client/routing.js
+++ client/routing.js
@@ -1,7 +1,18 @@
+Router.configure({
+	layoutTemplate: "ApplicationLayout"
+});
 Router.route('/', function () {
-  this.render('navbar');
+  this.render('welcome', { to: "main" });
 });
 
 Router.route('/images', function () {
-  this.render('images');
+  this.render('navbar', { to: "navbar" });
+  this.render('images', { to: "main" })
+});
+
+Router.route('/image/:_id', function () {
+  this.render('navbar', { to: "navbar" });
+  this.render('single-image', { to: "main", data: function() {
+  	return Images.findOne({_id: this.params._id});
+  }});
 });
